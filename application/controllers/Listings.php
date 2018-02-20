1923ce6cd19f24c8e7d228a36360b69bf2b62512
Thomas Buitelaar <99041950@mydavinci.nl>
2018-02-20 14:26:21 +0100

    Added basic interface and stylesheet

---
+++ application/controllers/Listings.php
@@ -0,0 +1,16 @@
+<?php
+class Listings extends CI_Controller 
+{
+    public function view($list = 'home')
+    {
+    	if(!file_exists(APPPATH.'views/listings/' . $list . '.php'))
+    	{
+    		show_404();
+    	}
+    	$data['title'] = ucfirst($list);
+
+    	$this->load->view('templates/header');
+    	$this->load->view('listings/' . $list, $data);
+    	$this->load->view('templates/footer');
+    }
+}
