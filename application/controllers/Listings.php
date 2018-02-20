0d43a4db060d9848a0b2e2809dff055d98489ee8
Thomas Buitelaar <99041950@mydavinci.nl>
2018-02-20 16:19:02 +0100

    updated controller listings.php

--- application/controllers/Listings.php
+++ application/controllers/Listings.php
@@ -1,16 +1,16 @@
 <?php
 class Listings extends CI_Controller 
 {
-    public function view($list = 'lists')
+    public function view($page = 'lists')
     {
-    	if(!file_exists(APPPATH.'views/listings/' . $list . '.php'))
+    	if(!file_exists(APPPATH.'views/listings/' . $page . '.php'))
     	{
     		show_404();
     	}
-    	$data['title'] = ucfirst($list);
+    	$data['title'] = ucfirst($page);
 
     	$this->load->view('templates/header');
-    	$this->load->view('listings/' . $list, $data);
+    	$this->load->view('listings/' . $page, $data);
     	$this->load->view('templates/footer');
     }
 }
